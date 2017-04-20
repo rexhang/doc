4c2283e08bd47088b9b2fc98fd2418eea0ed3064
hang rex <hang rex>
2017-04-20 13:08:27 +0800

    gitignore no nodemodules

--- gulp的使用/project/gulp-rev.js
+++ gulp的使用/project/gulp-rev.js
@@ -3,7 +3,7 @@ var rev = require('gulp-rev'),  // 生成添加版本号文件
 revReplace = require('gulp-rev-replace'); // 自动加入文件
 
 gulp.task('setTimeStamp', function(){
-    gulp.src('./css/index.css')
+    gulp.src('css/index.css')
         .pipe(gulp.dest('md5/css'))
                 .pipe(rev()) // 生成添加版本号文件
                 .pipe(gulp.dest('md5/css'))
